f6a57aaaea934dac2030dd9edaf0114b0c3c85be
Nickolay Mazurkin <mazurkin@gmail.com>
2017-07-01 10:33:37 +0300

    initial

--- src/test/java/org/test/SimpleMapTest.java
+++ src/test/java/org/test/SimpleMapTest.java
@@ -8,16 +8,24 @@ public class SimpleMapTest {
     public void test() throws Exception {
         SimpleMap<A, String> m1 = new SimpleMap<>();
         m1.put(new B(), "a");
-        handle(m1);
+        handle1(m1);
+        handle2(m1);
 
         SimpleMap<B, String> m2 = new SimpleMap<>();
         m2.put(new B(), "a");
-        handle(m2);
+        handle1(m2);
+        handle2(m2);
     }
 
-    private static void handle(SimpleMap<? extends A, String> m) {
+    private static void handle1(SimpleMap<? extends A, String> m) {
         m.get(null);
-        m.get(new A())
+        m.get(new A());
+        m.get(new B());
+    }
+
+    private static void handle2(SimpleMap<A, String> m) {
+        m.get(null);
+        m.get(new A());
         m.get(new B());
     }
 
